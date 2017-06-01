43e7eb9f217a4c5693bf6f33289dfc748b0e2eeb
pinheirochagas <pedropchagas@gmail.com>
2017-06-01 15:58:22 +0200

    some decoding

--- paper/calc_meg_paper.m
+++ paper/calc_meg_paper.m
@@ -8,7 +8,7 @@ InitDirsMEGcalc
 sub_name = {'s01','s02','s03','s04','s05','s06','s07','s08','s09','s10','s11','s12','s13','s14','s15', ...
         's16','s17','s18','s19','s21','s22'};
     
-sub_name = {'s14','s15','s16','s17','s18','s19','s21','s22'};    
+sub_name = {'s08','s09','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s21','s22'};    
 %% Behavior analysis
 behAnalysisCalcMEG(subs)
     
