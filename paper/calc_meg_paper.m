8e52d4a2d8b28da90103af7253b324be65bc1bcd
pinheirochagas <pedropchagas@gmail.com>
2017-06-15 15:16:03 +0200

    tiny rsa stats

--- paper/calc_meg_paper.m
+++ paper/calc_meg_paper.m
@@ -3,9 +3,12 @@
 %% Set paths and directories
 AddPathsMEGcalc
 InitDirsMEGcalc
+%% Get CoSMoMVPA
+cd(cosmo_mvpa_dir)
+cosmo_set_path()
 
 %%  List subjects
-sub_name = {'s02','s03','s04','s05','s06','s07','s08','s09','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s21','s22'};
+sub_name = {'s03','s04','s05','s06','s07','s08','s09','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s21','s22'};
     
 sub_name = {'s08','s09','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s21','s22'};    
 %% Behavior analysis
