b462e178444aa5b1dcd11e8a8333e8dd7df97c44
pinheirochagas <pedropchagas@gmail.com>
2017-07-17 10:17:16 +0200

    minor decoding

--- decoding/createJobsSoma.py
+++ decoding/createJobsSoma.py
@@ -4,11 +4,11 @@ sys.path.append('/neurospin/meg/meg_tmp/Calculation_Pedro_2014/scripts/decoding'
 from calc_createWorkFlow import (calc_createWorkFlow, calc_createWorkFlowRiemann)
 
 # Subjects
-subjects = ['s02', 's03', 's04', 's05', 's06', 's07', 's08', 's09', 's10',
-            's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's21', 's22']
+# subjects = ['s02', 's03', 's04', 's05', 's06', 's07', 's08', 's09', 's10',
+#             's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's21', 's22']
 
 # Subjects
-#subjects = ['s03']
+subjects = ['s03', 's09', 's21']
 
 #subjects = ['s03']
 conditions = [['op1','op1'], ['addsub', 'addsub'], ['op2', 'op2'], ['cres', 'cres'], ['resultlock_op1', 'resultlock_op1'],
@@ -19,22 +19,16 @@ conditions = [['op1','op1'], ['addsub', 'addsub'], ['op2', 'op2'], ['cres', 'cre
               ['resplock_op2', 'resplock_op2'], ['resplock_addsub', 'resplock_addsub'], ['resplock_op1', 'resplock_op1'], ['vsa', 'vsa'],
               ['vsa', 'addsub'], ['addsub', 'vsa'], ['resultlock_cres', 'cres'], ['op1', 'cres'], ['op1', 'resultlock_pres']]
 
-conditions = [['op1','op1'], ['op2', 'op2'], ['cres', 'cres'], ['resultlock_op1', 'resultlock_op1'],
-              ['resultlock_op2', 'resultlock_op2'], ['resultlock_cres', 'resultlock_cres'],
-              ['resultlock_pres', 'resultlock_pres'], ['resultlock_absdeviant', 'resultlock_absdeviant'], ['resultlock_deviant', 'resultlock_deviant'],
-              ['resplock_absdeviant', 'resplock_absdeviant'], ['resplock_deviant', 'resplock_deviant'], ['resplock_pres', 'resplock_pres'], ['resplock_cres', 'resplock_cres'],
-              ['resplock_op2', 'resplock_op2'], ['resplock_op1', 'resplock_op1'],
-              ['resultlock_cres', 'cres'], ['op1', 'cres'], ['op1', 'resultlock_pres']]
+conditions = [['resultlock_absdeviant', 'resultlock_absdeviant'], ['resultlock_deviant', 'resultlock_deviant'],
+              ['resplock_absdeviant', 'resplock_absdeviant'], ['resplock_deviant', 'resplock_deviant']]
 
-conditions = [['op2', 'op2'], ['resultlock_op2', 'resultlock_op2'], ['resplock_op2', 'resplock_op2']]
-
-#conditions = [['op1_riemann','op1_riemann']]
+conditions = [['op2','op2']]
 
 baselinecorr = 'nobaseline'
-dec_method = 'class'
-dec_scorer = 'accuracy'
-#dec_method = 'reg'
-#dec_scorer = 'kendall_score'
+# dec_method = 'class'
+# dec_scorer = 'accuracy'
+dec_method = 'reg'
+dec_scorer = 'kendall_score'
 gatordiag = 'gat'
 decimate = 2
 
